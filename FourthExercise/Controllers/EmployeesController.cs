5ce5f200517bbcf854b5d35d8ccf44c503e7c3d3
Zdravko Georgiev <zdravkogeorgiev@windowslive.com>
2016-10-10 23:13:20 +0300

    Add project files.

---
+++ FourthExercise/Controllers/EmployeesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FourthExercise.Models;
+
+namespace FourthExercise.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private FourthExerciseContext fourthExerciseContext = new FourthExerciseContext();
+
+        // GET: Employees
+        public async Task<ActionResult> Index()
+        {
+            return View(await fourthExerciseContext.Employees.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await fourthExerciseContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,FirstName,LastName,Email,Salary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                fourthExerciseContext.Employees.Add(employee);
+                await fourthExerciseContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await fourthExerciseContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,FirstName,LastName,Email,Salary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                fourthExerciseContext.Entry(employee).State = EntityState.Modified;
+                await fourthExerciseContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await fourthExerciseContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Employee employee = await fourthExerciseContext.Employees.FindAsync(id);
+            fourthExerciseContext.Employees.Remove(employee);
+            await fourthExerciseContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                fourthExerciseContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FourthExercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FourthExercise.MvcApplication" Language="C#" %>
