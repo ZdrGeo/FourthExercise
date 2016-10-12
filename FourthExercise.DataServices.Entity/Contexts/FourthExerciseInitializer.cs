c81aa2e60882b6eb33ae5cd1dd4d7a82d2b3132c
Zdravko Georgiev <zdravkogeorgiev@windowslive.com>
2016-10-12 09:28:52 +0300

    DB seed with employees

--- FourthExercise.DataServices.Entity/Contexts/FourthExerciseInitializer.cs
+++ FourthExercise.DataServices.Entity/Contexts/FourthExerciseInitializer.cs
@@ -8,7 +8,9 @@ using FourthExercise.Models;
 
 namespace FourthExercise.DataServices.Entity
 {
-    public class FourthExerciseInitializer : DropCreateDatabaseIfModelChanges<FourthExerciseContext> // DropCreateDatabaseIfModelChanges<FourthExerciseContext> // DropCreateDatabaseAlways<FourthExerciseContext>
+    // DropCreateDatabaseAlways<FourthExerciseContext>
+    // DropCreateDatabaseIfModelChanges<FourthExerciseContext>
+    public class FourthExerciseInitializer : DropCreateDatabaseAlways<FourthExerciseContext>
     {
         protected override void Seed(FourthExerciseContext context)
         {
@@ -19,6 +21,15 @@ namespace FourthExercise.DataServices.Entity
             jobRoles.Add(new JobRole() { Id = 3, Name = "Manager's Manager" });
 
             context.JobRoles.AddRange(jobRoles);
+
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(new Employee() { Id = 1, FirstName = "Petar", LastName = "Minkov", Email = "m_minkov@gmail.com", JobRoleId = 1, Salary = 3500 });
+            employees.Add(new Employee() { Id = 2, FirstName = "Ivan", LastName = "Petrov", Email = "i_petrov@gmail.com", JobRoleId = 1, Salary = 3500 });
+            employees.Add(new Employee() { Id = 3, FirstName = "Petar", LastName = "Ivanov", Email = "p_ivanov@gmail.com", JobRoleId = 2, Salary = 4000 });
+            employees.Add(new Employee() { Id = 4, FirstName = "Dimitar", LastName = "Kirilov", Email = "d_kirilov@gmail.com", JobRoleId = 3, Salary = 4500 });
+
+            context.Employees.AddRange(employees);
 
             base.Seed(context);
         }
