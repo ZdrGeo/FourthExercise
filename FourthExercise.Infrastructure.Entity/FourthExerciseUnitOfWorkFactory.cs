75aa3b866d41c6daa60817bfa972c91184c67021
Zdravko Georgiev <zdravkogeorgiev@windowslive.com>
2016-10-14 07:49:21 +0300

    Fix: Reject changes in uncompleted UOW

--- FourthExercise.Infrastructure.Entity/FourthExerciseUnitOfWorkFactory.cs
+++ FourthExercise.Infrastructure.Entity/FourthExerciseUnitOfWorkFactory.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.Entity;
 using System.Transactions;
+using System.Data.Entity.Infrastructure;
 
 namespace FourthExercise.Infrastructure.Entity
 {
@@ -20,13 +21,40 @@ namespace FourthExercise.Infrastructure.Entity
 
         private FourthExerciseContext context;
 
+        private void RejectChanges()
+        {
+            foreach (DbEntityEntry entry in context.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Modified:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                    default: break;
+                }
+            }
+        }
+
         public async Task WithAsync(Func<UnitOfWork, Task> action)
         {
             UnitOfWork unitOfWork = new UnitOfWork();
 
             await action(unitOfWork);
 
-            if (unitOfWork.IsCompleted) { await context.SaveChangesAsync(); }
+            if (unitOfWork.IsCompleted)
+            {
+                await context.SaveChangesAsync();
+            }
+            else
+            {
+                RejectChanges();
+            }
 
             // This should be the implementation in case that we need distributed transaction
             /*
@@ -41,6 +69,10 @@ namespace FourthExercise.Infrastructure.Entity
                     await context.SaveChangesAsync();
 
                     transactionScope.Complete();
+                }
+                else
+                {
+                    RejectChanges();
                 }
             }
             */
