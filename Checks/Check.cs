b0135c44d84d37fae4c1af0e8cb52cdb0ab9c752
Zdravko Georgiev <zdravko.georgiev@gmail.com>
2016-12-20 16:43:34 +0200

    Added Checks project

---
+++ Checks/Check.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checks
+{
+    public class Check
+    {
+        public static T NotNull<T>(T value, string parameterName) where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+
+        public static T? NotNull<T>(T? value, string parameterName) where T : struct
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+
+        public static string NotEmpty(string value, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("The parameter cannot be null, empty or contain only white space.", parameterName);
+            }
+
+            return value;
+        }
+    }
+}
